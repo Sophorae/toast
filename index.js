5cec157cae2f50e849e9156c0bf198d3e173bb66
jiaofuming <jiaofuming@iautos.cn>
2018-09-10 10:29:19 +0800

    add

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-function Toast(option) {
-    if (option) {
-        if (option.type == "success") {
-            document.createDocumentFragment("div");
-        }
-    } else {
-        console.error("(#`O′)未定义option, 格式:{type: \"success/failure\", tips: text}")
-    }
-
-}
-Toast()
-/* 
-{
+/*
+option = {
     type: "",
-
+    message: ""
 }
 */
+function Toast(option) {
+    var toast = $("<div>");
+    toast.css({
+        "position": "fixed",
+        "top": 0,
+        "left": 0,
+        "width": "100%",
+        "height": "100%",
+        "background": "rgba(0,0,0,0.35)",
+        "z-index": "100px"
+    })
+    if (option) {
+        
+    } else {
+        console.error("(#`O′)未定义option, 格式:{type: \"success/failure\", message: text}")
+    }
+    $("body").append(toast)
+}
+Toast()
